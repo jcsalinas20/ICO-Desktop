c539d664a2b43c7cb3819c8411094a155fb3322f
Marc Morales <marcmoraleslinares@gmail.com>
2020-03-09 19:56:34 +0100

    Interfaz de búsqueda

--- src/init/LoginController.java
+++ src/init/LoginController.java
@@ -1,16 +1,22 @@
 package init;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import conexion.MongoActions;
 import interfaz.Interfaz;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import programa.Data;
 
-public class LoginController {
-	
+public class LoginController implements Initializable {
+		
 	@FXML
 	private TextField login_user;
 	
@@ -23,8 +29,23 @@ public class LoginController {
 	@FXML
 	private Label login_error;
 	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {	
+		EventHandler<KeyEvent> enterClick = new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				String tecla = event.getCode().getName();
+				if(tecla.equals("Enter")) {
+					controlarLogin();
+				}
+			}
+		};
+		login_user.setOnKeyPressed(enterClick);
+		login_password.setOnKeyPressed(enterClick);
+	}
 	
-	public void controlarClick() {
+		
+	public void controlarLogin() {
 		MongoActions mongo = new MongoActions();
 		String user = login_user.getText();
 		String pass = login_password.getText();
@@ -61,4 +82,5 @@ public class LoginController {
 		login_password.setText("");
 		login_error.setVisible(true);
 	}
+	
 }
--- src/interfaz/SearchController.java
+++ src/interfaz/SearchController.java
@@ -4,29 +4,39 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.ResourceBundle;
+
+
 import java.util.Map.Entry;
 
 import clases.Paciente;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import programa.Data;
 
 public class SearchController implements Initializable {
 
 	private HashMap<String, Paciente> pacientes;
 	
-	private ArrayList<String> nombresPacientes;
+	private ObservableList<AnchorPane> items;
 
 	@FXML
 	private TextField search_text;
 
 	@FXML
-	private ListView<String> pacientes_list;
+	private ListView<AnchorPane> pacientes_list;
 
 	@FXML
 	private ImageView paciente_image;
@@ -34,24 +44,65 @@ public class SearchController implements Initializable {
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		pacientes = Data.pacientes;
-		nombresPacientes = new ArrayList<>();
+		items = FXCollections.observableArrayList(new ArrayList<AnchorPane>());
+		pacientes_list.setItems(items);
 		busqueda(); //CARGO TODOS LOS PACIENTES
 	}
 
 	public void busqueda() {
-		pacientes_list.getItems().removeAll(nombresPacientes); //BORRO TODO EL CONTENIDO DE LA LISTA
-		nombresPacientes = new ArrayList<>();
+		//BORRO TODO EL LISTADO DE PACIENTES
+		items.removeAll();
+		pacientes_list.setItems(items);
 		
+		//BUSCO LOS PACIENTES
+		ArrayList<AnchorPane> anchorPacientes = new ArrayList<>(); //RECOGE LOS ANCHOR PANE DE CADA PACIENTE
 		String textoBusqueda = search_text.getText(); //RECOJO EL TEXTO ESCRITO POR EL USUARIO
 		String nombrePos;
+		AnchorPane anchorPos;
 		for(Entry<String, Paciente> entry : pacientes.entrySet()) {
 			nombrePos = entry.getKey(); //RECORRO EL MAPA RECOGIENDO LOS NOMBRES DE PACIENTES
 			if(nombrePos.startsWith(textoBusqueda)) {
-				nombresPacientes.add(nombrePos); //SI EMPIEZAN POR EL TEXTO ESCRITO POR EL USUARIO LOS AÑADO A UNA LISTA
+				anchorPos = castToAnchor(entry.getValue());
+				anchorPacientes.add(anchorPos); //AÑADO LOS PACIENTES QUE COINCIDAN CON LA BUSQUEDA AL ARRAYLIST
 			}
 		}
-		ObservableList<String> items = FXCollections.observableArrayList(nombresPacientes);
-		pacientes_list.setItems(items); //AÑADO LOS NOMBRES DE LA LISTA AL LISTVIEW
+		
+		items = FXCollections.observableArrayList(anchorPacientes);
+		pacientes_list.setItems(items); //AÑADO LOS PACIENTES DE LA LISTA AL LISTVIEW
 	}
 	
+	private AnchorPane castToAnchor(Paciente p) {
+		AnchorPane anchor = new AnchorPane();
+		
+		//PONGO LA IMAGEN DEL PACIENTE
+		ImageView pacienteImage = new ImageView();
+		Image imagenPaciente = new Image(p.getFoto());
+		pacienteImage.setImage(imagenPaciente);
+		pacienteImage.setX(5);
+		pacienteImage.setFitHeight(80);
+		pacienteImage.setFitWidth(80);
+		
+		//PONGO EL NOMBRE DEL PACIENTE
+		Label nombrePaciente = new Label(p.getNombre() + " " + p.getApellidos());
+		nombrePaciente.setLayoutX(100);
+		nombrePaciente.setLayoutY(32);
+		nombrePaciente.setFont(Font.font("SansSerif", FontWeight.BOLD, FontPosture.REGULAR, 18));
+
+		//AÑADO LA IMAGE Y EL LABEL AL PANE
+		anchor.getChildren().add(pacienteImage);
+		anchor.getChildren().add(nombrePaciente);
+		
+		anchor.setOnMouseClicked(new EventHandler<Event>() {
+			@Override
+			public void handle(Event event) {
+				showDataPaciente(p, imagenPaciente);
+			}
+		});
+				
+		return anchor;
+	}
+	
+	private void showDataPaciente(Paciente p, Image pacienteImage) {
+		paciente_image.setImage(pacienteImage);
+	}
 }
